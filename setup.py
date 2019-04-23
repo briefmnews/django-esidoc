76e694613095e50b6c5dfe49a4f033fb55910ebb
Thomas C <thomas.carbillet@gmail.com>
2019-04-23 16:43:59 +0200

    chore(): setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(name='django-esidoc',
+      version='0.1.0',
+      description='Handle login and ticket validation for e-sidoc',
+      url='https://github.com/briefmnews/django-esidoc',
+      author='Brief.me',
+      author_email='tech@brief.me',
+      license='GNU GPL v3',
+      packages=['django_esidoc', 'django_esidoc.migrations',],
+      python_requires="==2.7.*",
+      install_requires=[
+        'Django>=1.11',
+        'python-cas==1.4.0',
+        'lxml>=4.2.5',
+        'requests>=2.19.1',
+      ],
+      zip_safe=False)
