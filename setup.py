0f08b18ef2d38108a7850f8f79885b4cbc575c1c
Thomas C <thomas.carbillet@gmail.com>
2019-09-02 14:55:43 +0200

    feat(release): version 0.1.5

--- setup.py
+++ setup.py
@@ -2,16 +2,16 @@ from setuptools import setup
 
 setup(
     name="django-esidoc",
-    version="0.1.4",
+    version="0.1.5",
     description="Handle login and ticket validation for e-sidoc",
     url="https://github.com/briefmnews/django-esidoc",
     author="Brief.me",
     author_email="tech@brief.me",
     license="GNU GPL v3",
     packages=["django_esidoc", "django_esidoc.migrations"],
-    python_requires=">=2.7",
+    python_requires=">=3.7",
     install_requires=[
-        "Django>=1.11",
+        "Django>=2.2",
         "djangorestframework>=3.8.2",
         "python-cas==1.4.0",
         "lxml>=4.2.5",
