42d9a7701b4729ce88b9a55f4ae136ffabe77202
Thomas C <thomas.carbillet@gmail.com>
2020-03-13 14:30:05 +0100

    feat(admin): batch add institution command in the admin #9

--- django_esidoc/admin.py
+++ django_esidoc/admin.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
+from django.urls import path
+
+from functools import update_wrapper
 
 from .gar import delete_gar_subscription
-from .forms import InstitutionForm
+from .forms import BatchAddInstitutionForm, BatchAddInstitutionsFormPreview, InstitutionForm
 from .models import Institution
 
 
 class InstitutionAdmin(admin.ModelAdmin):
+    change_list_template = "django_esidoc/admin/change_list.html"
+
     raw_id_fields = ("user",)
     list_display = ("institution_name", "ent", "user", "uai", "ends_at")
     list_select_related = ("user",)
@@ -17,6 +22,26 @@ class InstitutionAdmin(admin.ModelAdmin):
         if obj.ent == "GAR":
             delete_gar_subscription(obj.uai)
         super().delete_model(request, obj)
+
+    def get_urls(self):
+
+        urls = super().get_urls()
+
+        def wrap(view):
+            def wrapper(*args, **kwargs):
+                return self.admin_site.admin_view(view)(*args, **kwargs)
+
+            return update_wrapper(wrapper, view)
+
+        custom_urls = [
+            path(
+                "batch_add/",
+                wrap(BatchAddInstitutionsFormPreview(BatchAddInstitutionForm)),
+                name="add_institutions",
+            ),
+        ]
+
+        return custom_urls + urls
 
 
 admin.site.register(Institution, InstitutionAdmin)
--- django_esidoc/forms.py
+++ django_esidoc/forms.py
@@ -1,9 +1,18 @@
+import datetime
+import re
 import requests
 
 from datetime import datetime
+from formtools.preview import FormPreview
 
+from django import forms
 from django.conf import settings
+from django.contrib import messages
+from django.contrib.auth import get_user_model
 from django.forms import ModelForm, ValidationError
+from django.shortcuts import redirect
+from django.urls import reverse
+from django.utils.translation import ugettext_lazy as _
 
 from .gar import (
     delete_gar_subscription,
@@ -17,6 +26,7 @@ from .models import Institution
 ENT_GAR_DISTRIBUTOR_ID = getattr(settings, "ENT_GAR_DISTRIBUTOR_ID", "")
 ENT_GAR_RESOURCES_ID = getattr(settings, "ENT_GAR_RESOURCES_ID", "")
 ENT_GAR_ORGANIZATION_NAME = getattr(settings, "ENT_GAR_ORGANIZATION_NAME", "")
+User = get_user_model()
 
 
 class InstitutionForm(ModelForm):
@@ -107,3 +117,85 @@ class InstitutionForm(ModelForm):
             xml = xml.replace("<uaiEtab>{uai}</uaiEtab>".format(uai=uai), "")
 
         return xml
+
+
+class BatchAddInstitutionForm(forms.Form):
+    institutions_data = forms.CharField(
+        widget=forms.Textarea,
+        required=True,
+        help_text=_(
+            "Copy/paste the csv with the following format: email "
+            "(mandatory), uai (mandatory), ent (mandatory), intitution name (mandatory"
+        ),
+    )
+
+    def clean_institutions_data(self):
+        institutions_data = self._get_institution_fields(self.cleaned_data["institutions_data"])
+        for institution_detail in institutions_data:
+            ent = institution_detail[2]
+            ent_choices = [val[0] for val in Institution.ENVIRONNEMENTS_NUMERIQUES_DE_TRAVAIL]
+            if ent not in ent_choices:
+                raise ValidationError(_('ENTs must take ine of the following values: {}'.format(ent_choices)))
+
+        return institutions_data
+
+    @staticmethod
+    def _get_institution_fields(institutions_data):
+        sep = re.compile(r"[,\t]")
+        ret = []
+
+        for data in institutions_data.split("\n"):
+            fields = sep.split(data)
+            try:
+                email = fields[0].lower().strip()
+            except IndexError:
+                raise ValidationError(_('Email is madatory'))
+            try:
+                uai = fields[1].upper().strip()
+            except IndexError:
+                raise ValidationError(_('Uai number is madatory'))
+            try:
+                ent = fields[2].upper().strip()
+            except IndexError:
+                raise ValidationError(_('Ent is madatory'))
+            try:
+                institution_name = fields[3].strip()
+            except IndexError:
+                raise ValidationError(_('Institution name is madatory'))
+
+            ret.append((email, uai, ent, institution_name))
+
+        return ret
+
+
+class BatchAddInstitutionsFormPreview(FormPreview):
+    preview_template = "django_esidoc/admin/preview.html"
+    form_template = "django_esidoc//admin/form.html"
+
+    def process_preview(self, request, form, context):
+        emails = [email for email, _, _, _ in form.cleaned_data["institutions_data"]]
+        existing_users = User.objects.filter(email__in=emails)
+        context["users_found_count"] = existing_users.count()
+        context["users_not_found_count"] = len(emails) - existing_users.count()
+
+        return context
+
+    def done(self, request, cleaned_data):
+        institutions_data = cleaned_data["institutions_data"]
+
+        for institution_detail in institutions_data:
+            try:
+                user = User.objects.get(email=institution_detail[0])
+            except User.DoesNotExist:
+                continue
+
+            Institution.objects.create(user=user, uai=institution_detail[1], ent=institution_detail[2], institution_name=institution_detail[3], ends_at=datetime.now())
+
+        messages.info(
+            request,
+            (
+                "La commande a bien été prise en compte."
+            ),
+        )
+
+        return redirect(reverse("admin:django_esidoc_institution_changelist"))
