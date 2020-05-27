b2517af9a99c60dc510563c24caea9de3c65fb34
Thomas C <thomas.carbillet@gmail.com>
2020-05-27 12:16:43 +0200

    feat(gar): add get_gar_subscription_end_date function

--- django_esidoc/gar.py
+++ django_esidoc/gar.py
@@ -1,10 +1,14 @@
 import requests
 
+from bs4 import BeautifulSoup
+
 from django.conf import settings
 
 
 ENT_GAR_SUBSCRIPTION_PREFIX = getattr(settings, "ENT_GAR_SUBSCRIPTION_PREFIX", "")
 ENT_GAR_BASE_SUBSCRIPTION_URL = getattr(settings, "ENT_GAR_BASE_SUBSCRIPTION_URL", "")
+
+ENT_GAR_DISTRIBUTOR_ID = getattr(settings, "ENT_GAR_DISTRIBUTOR_ID", "")
 
 ENT_GAR_CERTIFICATE_PATH = getattr(settings, "ENT_GAR_CERTIFICATE_PATH", "")
 ENT_GAR_KEY_PATH = getattr(settings, "ENT_GAR_KEY_PATH", "")
@@ -41,3 +45,29 @@ def get_gar_certificate():
 def get_gar_headers():
     headers = {"Content-Type": "application/xml"}
     return headers
+
+
+def get_gar_subscription_end_date(uai):
+    data = """<?xml version="1.0" encoding="UTF-8"?>
+    <filtres xmlns="http://www.atosworldline.com/wsabonnement/v1.0/">
+          <filtre>
+                <filtreNom>idDistributeurCom</filtreNom>
+                <filtreValeur>{distributor_id}</filtreValeur>
+          </filtre> 
+          <filtre>
+                <filtreNom>uaiEtab</filtreNom>
+                <filtreValeur>{uai}</filtreValeur>
+          </filtre> 
+    </filtres>""".format(distributor_id=ENT_GAR_DISTRIBUTOR_ID, uai=uai)
+    response = requests.request(
+        "GET",
+        "https://abonnement.gar.education.fr/abonnements",
+        data=data,
+        cert=get_gar_certificate(),
+        headers=get_gar_headers(),
+    )
+    soup = BeautifulSoup(response.text, "lxml")
+    subscriptions = soup.findAll("abonnement")
+    for subscription in subscriptions:
+        if subscription.find("idabonnement").text == get_gar_subscription_id(uai):
+            return subscription.find("finvalidite").text
