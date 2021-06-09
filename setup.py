8357051cb9980262213448e81d02897f390a000c
Thomas <thomas.carbillet@gmail.com>
2021-06-09 16:37:49 +0200

    feat: release version 2.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="django-esidoc",
-    version="1.5.3",
+    version="2.0.0",
     description="Handle login and ticket validation for french ent (Esidoc, HDF and Occitanie)",
     url="https://github.com/briefmnews/django-esidoc",
     author="Brief.me",
