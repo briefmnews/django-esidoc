80752b9bdb530b53c103c1e853deac3ce1d0e810
Thomas <tcarbillet@brief.me>
2023-02-27 15:22:06 +0100

    refactor: using from django.utils.translation import ugettext_lazy is deprecated

--- django_esidoc/forms.py
+++ django_esidoc/forms.py
@@ -10,7 +10,7 @@ from django.contrib.auth import get_user_model
 from django.forms import ModelForm, ValidationError
 from django.shortcuts import redirect
 from django.urls import reverse
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 from .models import Institution
 
