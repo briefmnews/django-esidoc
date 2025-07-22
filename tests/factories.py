fd46843b2e1b388fb463612c7f9a5ff497588c90
Thomas Lenovo T14 <tcarbillet@brief.me>
2025-07-22 14:06:38 +0200

    test: fix warning DeprecationWarning: UserFactory._after_postgeneration will stop saving the instance after postgeneration hooks in the next major release.

--- tests/factories.py
+++ tests/factories.py
@@ -18,6 +18,7 @@ class InstitutionFactory(factory.django.DjangoModelFactory):
 class UserFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = get_user_model()
+        skip_postgeneration_save = True
 
     email = factory.Sequence(lambda n: "noel{0}@flantier.com".format(n))
     is_active = True
